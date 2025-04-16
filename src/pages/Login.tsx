4d3917e6a9bab39cbddaab56c4420171b62622da
HeytorPires <heytor.cacho29@gmail.com>
2025-04-16 13:18:49 -0300

    :construction: style: auth

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,13 @@
+import DecryptedText from "@/components/framer/framer-motion";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Toaster } from "@/components/ui/sonner";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { authService } from "@/service/authService";
 import { AlertCircle } from "lucide-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-type ErrorRequest = { AuthApiError: string; Error: boolean };
+import { toast } from "sonner";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -21,10 +22,12 @@ const Login = () => {
     }
     try {
       const { data, error } = await authService.signIn(email, password);
-      console.log(data);
-      console.log(error);
-      setError(error?.message);
-      console.log(error);
+      if (error) {
+        toast.error(error.message);
+        return;
+      }
+      console.log("Usuário logado com sucesso:", data);
+      navigate("/home");
     } catch (err: any) {
       setError(err);
     }
@@ -41,12 +44,17 @@ const Login = () => {
           <AlertDescription>{error}</AlertDescription>
         </Alert>
       )}
+      <Toaster />
       <form
         className="flex flex-col justify-center items-center gap-4 p-4 w-96 "
         onSubmit={handleSubmit}
       >
         <h1 className="text-8xl">Taskly</h1>
-        <p>Suas tarefas de maneira mais fácil</p>
+        <DecryptedText
+          text="Suas tarefas de maneira mais fácil"
+          speed={75}
+          animateOn="view"
+        />
         <h2 className="text-3xl">Entre</h2>
         <Input
           type="email"
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,30 +1,85 @@
+import DecryptedText from "@/components/framer/framer-motion";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { authService } from "@/service/authService";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Register = () => {
-  const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
+  const navigate = useNavigate();
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // previne o comportamento padrão
+    if (!email || !password) {
+      toast.error("Preencha todos os dados");
+    }
+    try {
+      const { data, error } = await authService.signUp(email, password);
+      if (error) {
+        toast.error(error.message);
+        return;
+      }
+      console.log("Usuário logado com sucesso:", data);
+      navigate("/home");
+    } catch (err: any) {
+      toast.error(err);
+    }
+  };
   const toLogin = () => {
     navigate("/login");
   };
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="flex flex-col justify-center items-center gap-4 p-4 w-96 ">
-        <h1 className="text-8xl">Taskly</h1>
-        <p>Sua tarefas de maneira mais fácil</p>
-        <h2 className="text-5xl">Entre</h2>
-
-        <Input type="text" placeholder="Escreva seu nome" />
-        <Input type="email" placeholder="Escreva seu email" />
-        <Input type="password" placeholder="Escreva sua senha" />
-        <p className="text-[10px]">
-          Já tem uma conta? Clique{" "}
-          <span className="text-blue-700 underline" onClick={toLogin}>
-            Aqui
-          </span>
-        </p>
-        <Button type="submit">Cadastrar</Button>
+        <form
+          className="flex flex-col justify-center items-center gap-4 p-4 w-96 "
+          onSubmit={handleSubmit}
+        >
+          <h1 className="text-8xl">Taskly</h1>
+          <DecryptedText
+            text="Suas tarefas de maneira mais fácil"
+            speed={75}
+            animateOn="view"
+          />
+          <h2 className="text-3xl">Cadastre-se</h2>
+          <Input
+            type="text"
+            placeholder="Escreva seu nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)} // também atualiza
+            required
+          />
+          <Input
+            type="email"
+            placeholder="Escreva seu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} // atualiza o state
+            required
+          />
+          <Input
+            type="password"
+            placeholder="Escreva sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} // também atualiza
+            required
+          />
+          <p className="text-[10px]">
+            já tem conta cadastrada? Clique{" "}
+            <span
+              className="text-blue-700 underline cursor-pointer"
+              onClick={toLogin}
+            >
+              Aqui
+            </span>
+          </p>
+          <Button className="cursor-pointer" type="submit">
+            Cadastrar-se
+          </Button>
+        </form>
       </div>
     </div>
   );
