d015eaac2d4609ed1bee5b6e51294828f7860a50
HeytorPires <heytor.cacho29@gmail.com>
2025-04-22 17:44:37 -0300

    :bug: feat: no render sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,14 +4,12 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  useLocation,
   Navigate,
 } from "react-router-dom";
 import { AuthProvider } from "@/contexts/auth";
 import { useAuth } from "@/hooks/useAuth";
 import { Toaster } from "sonner";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 // Pages
 import Login from "@/pages/Login";
@@ -19,6 +17,7 @@ import Register from "@/pages/Register";
 import Home from "@/pages/Home";
 import Tasks from "@/pages/Tasks";
 import NotFound from "@/pages/NotFound";
+import AppLayout from "@/components/layout/layout";
 
 interface PrivateProps {
   Item: React.ElementType;
@@ -29,58 +28,47 @@ const Private: React.FC<PrivateProps> = ({ Item }) => {
   return user ? <Item /> : <Navigate to="/login" replace />;
 };
 
-const AppLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const location = useLocation();
-  const shouldShowSidebar = !["/login", "/register", "/not-found"].includes(
-    location.pathname
-  );
-
-  return (
-    <>
-      {shouldShowSidebar && <AppSidebar />}
-      <main
-        className={`w-full min-h-screen flex ${
-          shouldShowSidebar ? "" : "justify-center items-center"
-        }`}
-      >
-        {shouldShowSidebar && <header>{<SidebarTrigger />}</header>}
-        <div className={`flex-grow ${shouldShowSidebar ? "ml-..." : ""}`}>
-          {children}
-        </div>
-      </main>
-    </>
-  );
-};
-
 // Esse componente agora apenas define as rotas dentro do layout apropriado
 const AppRoutes: React.FC = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div></div>; // Ou um componente de loading mais elaborado
+  }
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Private Item={Home} />} />
-      <Route path="/tasks" element={<Private Item={Tasks} />} />
-      <Route path="/not-found" element={<NotFound />} />
-      <Route path="*" element={<Navigate to="/not-found" replace />} />{" "}
-      {/* Redireciona rotas não encontradas para /not-found */}
+      {user ? (
+        <>
+          <Route path="/home" element={<Private Item={Home} />} />
+          <Route path="/tasks" element={<Private Item={Tasks} />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/not-found" replace />} />{" "}
+          {/* Redireciona rotas não encontradas para /not-found */}
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />{" "}
+        </>
+      )}
     </Routes>
   );
 };
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <AuthProvider>
-        <SidebarProvider>
-          <AppLayout>
-            <AppRoutes />
-          </AppLayout>
-          <Toaster />
-        </SidebarProvider>
-      </AuthProvider>
-    </Router>
-  );
-};
+const App: React.FC = () => (
+  <Router>
+    <AuthProvider>
+      <SidebarProvider>
+        <AppLayout>
+          <AppRoutes />
+        </AppLayout>
+        <Toaster />
+      </SidebarProvider>
+    </AuthProvider>
+  </Router>
+);
 
 export default App;
---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { useAuth } from "@/hooks/useAuth"; // Importe o useAuth aqui
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
+interface AppLayoutProps {
+  children: React.ReactNode;
+}
+
+const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
+  const location = useLocation();
+  const { user, loading } = useAuth(); // Obtenha loading do useAuth
+  const shouldShowSidebar =
+    !loading &&
+    !["/login", "/register", "/not-found"].includes(location.pathname) &&
+    user;
+
+  if (loading) {
+    return <div></div>; // Ou seu componente de loading
+  }
+
+  return (
+    <>
+      {shouldShowSidebar && <AppSidebar />}
+      <main
+        className={`w-full min-h-screen flex ${
+          shouldShowSidebar ? "" : "justify-center items-center"
+        }`}
+      >
+        {shouldShowSidebar && <header>{<SidebarTrigger />}</header>}
+        <div className={`flex-grow ${shouldShowSidebar ? "ml-..." : ""}`}>
+          {children}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default AppLayout;
