010cde223606cdefd3e90e09dcfdca07cc7ac2eb
HeytorPires <heytor.cacho29@gmail.com>
2025-05-19 14:16:14 -0300

    :rocket: feat: to production

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/nimbus/", // <- ESSENCIAL para GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
